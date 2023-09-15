da550454b79bedca631a22e9af4be2a8702bc01b
MuazzezA <ayd.muazzez@gmail.com>
2023-09-15 17:41:07 +0300

    add 3 page to HomeNC

---
+++ NewsApp/Views/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  NewsApp
+//
+//  Created by Muazzez Aydın on 15.09.2023.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+
+}
