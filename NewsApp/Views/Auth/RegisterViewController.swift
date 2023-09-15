c7bebfa2b4728ef10a5c1d109427ba6ecb45adee
MuazzezA <ayd.muazzez@gmail.com>
2023-09-15 16:12:18 +0300

    feat: done register

--- NewsApp/Views/Auth/RegisterViewController.swift
+++ NewsApp/Views/Auth/RegisterViewController.swift
@@ -6,9 +6,21 @@
 //
 
 import UIKit
-
+import Firebase
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var mailRegisterTextField: UITextField!
+    @IBOutlet weak var pswRegisterTextField: UITextField!
+    
+    var user = false {
+        didSet {
+            if(user){
+                print("yönlendirme")
+                self.navigationController?.popViewController(animated: true)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +28,36 @@ class RegisterViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
+    @IBAction func registerButtonAct(_ sender: Any) {
+        guard let email = mailRegisterTextField.text, let password = pswRegisterTextField.text else {
+            // E-posta ve şifre alanlarının dolu olduğundan emin olun
+            return
+        }
+            
+        Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+            if let error = error {
+                
+                print("Kullanıcı kaydı başarısız: \(error.localizedDescription)")
+                if error.localizedDescription == "The email address is already in use by another account." {
+                    let alertController = UIAlertController(title: "Hata", message: "Bu e-posta adresi zaten başka bir hesap tarafından kullanılıyor.", preferredStyle: .alert)
+                    let okAction = UIAlertAction(title: "Tamam", style: .default, handler: nil)
+                    alertController.addAction(okAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+                self.user = false
+            } else {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+                let alertController = UIAlertController(title: "Başarılı", message: "Kayıt oldunuz! Artık giriş yapabilirsiniz.", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "Giriş Yap", style: .default) { (action) in
+                    self.user = true
+                }
+                alertController.addAction(okAction)
+                self.present(alertController, animated: true, completion: nil)
+
+            }
+        }
     }
-    */
-
+   
+    
+    
 }
