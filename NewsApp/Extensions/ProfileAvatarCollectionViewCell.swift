ed9a6e199c558ee6c41a78bd98c9eae400ab30a7
MuazzezA <ayd.muazzez@gmail.com>
2023-09-17 15:23:14 +0300

    feat : choose avatar collection view to profile

---
+++ NewsApp/Extensions/ProfileAvatarCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileAvatarCollectionViewCell.swift
+//  NewsApp
+//
+//  Created by Muazzez Aydın on 17.09.2023.
+//
+
+import UIKit
+
+class ProfileAvatarCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatar: UIImageView!
+    
+}
--- NewsApp/Views/Profile/ProfileViewController.swift
+++ NewsApp/Views/Profile/ProfileViewController.swift
@@ -9,21 +9,35 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var avatarImage: UIImageView!
+    @IBOutlet weak var avatarCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        avatarImage.image = UIImage(named: UserDefaults.standard.string(forKey: "avatar") ?? "no-image")
+        avatarCollectionView.delegate = self
+        avatarCollectionView.dataSource = self
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+extension ProfileViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 9
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "avatarCell", for: indexPath) as! ProfileAvatarCollectionViewCell
+        cell.avatar.image = UIImage(named: "\(indexPath.row + 1)")
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        avatarImage.image = UIImage(named: "\(indexPath.row + 1)")
+        UserDefaults.standard.set("\(indexPath.row + 1)", forKey: "avatar")
+    }
+    
 }
